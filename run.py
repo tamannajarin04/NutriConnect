7bc22cd8879f0cd9c153e68586d8d0f24a5296ee
Tamanna Jarin Tima <your_email@example.com>
2026-03-05 12:21:21 +0600

    Minor Comment Removing Modification

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from app import create_app
-from app.models import db, User, Role, DietaryPreference, BMIRecord  # ✅ added BMIRecord
+from app.models import db, User, Role, DietaryPreference, BMIRecord  
 
 app = create_app(os.getenv("FLASK_ENV") or "default")
 
@@ -15,7 +15,7 @@ def make_shell_context():
         "User": User,
         "Role": Role,
         "DietaryPreference": DietaryPreference,
-        "BMIRecord": BMIRecord  # ✅ added BMIRecord
+        "BMIRecord": BMIRecord  
     }
 
 if __name__ == "__main__":
